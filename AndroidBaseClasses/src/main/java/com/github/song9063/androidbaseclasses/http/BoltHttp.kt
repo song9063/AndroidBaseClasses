cab78ce9ff0b563c3ae85026d07e54a1714e0d35
Song Junwoo <busang.song@gmail.com>
2021-01-01 20:39:15 +0900

    Put

--- AndroidBaseClasses/src/main/java/com/github/song9063/androidbaseclasses/http/BoltHttp.kt
+++ AndroidBaseClasses/src/main/java/com/github/song9063/androidbaseclasses/http/BoltHttp.kt
@@ -36,4 +36,29 @@ class BoltHttp(context: Context) {
         BoltVolleyQueue.getInstance(mContext).addToRequestQueue(jsonObjReq)
     }
 
+    fun httpPut(strUrl: String, params: JSONObject,
+                onResp: (String, JSONObject, Boolean, Int) -> (Boolean),
+                onError: (String, String, Int) -> (Boolean),
+                extHeaders: HashMap<String, String>,
+                nCustomTag: Int = 0){
+
+        val jsonObjReq = object : JsonObjectRequest(
+                Request.Method.PUT, strUrl, params,
+                Response.Listener<JSONObject> { resp ->
+                    onResp(strUrl, resp, resp != null, nCustomTag)
+                },
+                Response.ErrorListener { error ->
+                    onError(strUrl, error.toString(), nCustomTag)
+                }
+        ){
+            override fun getHeaders(): MutableMap<String, String> {
+                return extHeaders
+                /*val headers = HashMap<String, String>()
+                headers.put("X-API-KEY",DLConst.HTTP_API_KEY)
+                return headers*/
+            }
+        }
+        BoltVolleyQueue.getInstance(mContext).addToRequestQueue(jsonObjReq)
+    }
+
 }
